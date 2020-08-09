cf5a300bc55fe15edad4b3b8148f763311c5e2a9
RukiyeDogru <66996656+RukiyeDogru@users.noreply.github.com>
2020-08-09 20:12:54 +0300

    Added StokTakip

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(int sayfa =1)
+        {
+            //var degerler = db.TBLKATEGORILER.ToList();
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa, 4);
+
+            return View(degerler);
+        }
+        [HttpGet]//Sayfa yükleyince bunu yap
+        public ActionResult YeniKategori ()
+        {
+            return View();
+        }
+        [HttpPost]//ben butona tıklayınca bunu yap
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if(!ModelState.IsValid)//doğrulama  işlemi yapılmadıysa
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Kategori");
+        }
+        public ActionResult SIL(int id)
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //güncelleme işleminin temeli olan formlar arası geçişi sağlıcaz
+        public ActionResult KategoriGetir(int id)//idmize göre kategorimizi getirsin
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", ktgr);
+        }
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(string p)
+        {
+            var degerler = from d in db.TBLMUSTERILER select d;//değerleri d üstünden alıyor direk listeyi döndürüyor.Parametre boşsa bunu yap değilse ifle olan komuta git
+            if(!string.IsNullOrEmpty(p))
+            {
+                //Eğerki p değeri boş değilse vey NULL DEĞİLSE o zaman burdaki işlemi gerçekleştir. Ama listeyi döndürücek (yukardaki)
+                degerler = degerler.Where(m => m.MUSTERIAD.Contains(p));//müşteri adı içerisinde parametreye eşit olan değerleri bana getirecek
+
+            }
+            return View(degerler.ToList());
+
+           // var degerler = db.TBLMUSTERILER.ToList();
+           // return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            if (!ModelState.IsValid)//doğrulama  işlemi yapılmadıysa
+            {
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult SIL(int id)
+        {
+            var musteri = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mus);
+
+        }
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERIAD = p1.MUSTERIAD;
+            musteri.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        
+        }
+    }
+}
